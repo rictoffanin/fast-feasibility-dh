2446557afcff74aa63ad38095298409e185dbbc4
Riccardo Toffanin <riccardo.toffanin@supsi.ch>
2021-08-19 16:37:56 +0200

    first trial to divide the paths

--- topology_finder.py
+++ topology_finder.py
@@ -2,7 +2,7 @@ import osmnx as ox
 import matplotlib.pyplot as plt
 import geopandas as gpd
 from pyproj import CRS
-from shapely.geometry import LineString, Point
+from shapely.geometry import LineString, Point, MultiLineString
 
 # Specify the name that is used to search for the data
 place_name = "Lugano, Ticino, Switzerland"
@@ -88,16 +88,28 @@ route = [i for i in route if i]
 # route_nodes = nodes.loc[route]
 # route_lines = nodes.loc[route[0]].unary_union
 route_lines = []
+# route_gdf = gpd.GeoDataFrame(columns=nodes.columns)
+
 for i in route:
     # print(nodes.loc[i])
     route_lines.append(LineString(list(nodes.loc[i].geometry.values)))
-
-print(route_lines)
-
+# flatten the list of lists containing the osmids
+flat_list = [item for sublist in route for item in sublist]
+# creating a GeoDataFrame with the nodes in the network
+route_nodes_gdf = nodes.loc[flat_list]
+# route_edges_gdf = edges['osmid'].loc[flat_list]
+print(edges)
+# dropping duplicates
+# route_edges_gdf.drop_duplicates(inplace=True)
+route_nodes_gdf.drop_duplicates(inplace=True)
+# print(route_lines)
+# print(route_gdf)
+route_graph = ox.graph_from_gdfs(route_nodes_gdf, edges)
+print(route_graph)
 route_geom = gpd.GeoDataFrame(route_lines, crs=edges.crs, columns=['geometry'])
 print(route_geom)
 # route_paths.plot()
-
+route_joined = LineString(route_nodes_gdf.geometry.unary_union)
 # Create a geometry for the shortest path
 # route_line = LineString(list(route_nodes.geometry.values))
 # print(route_nodes.loc[1, 'geometry'].values)
@@ -118,13 +130,17 @@ route_geom['length_m'] = route_geom.length
 
 # Plot edges and nodes
 ax = edges.plot(linewidth=0.75, color='gray')
-ax = nodes.plot(ax=ax, markersize=2, color='gray')
 
+ax = nodes.plot(ax=ax, markersize=2, color='gray')
+# route_aaa = gpd.GeoDataFrame(columns=['geometry'])
+# route_aaa.loc[0, 'geometry'] = route_joined
+# route_aaa.set_crs(crs=nodes.crs)
+# ax = route_aaa.plot(ax=ax, linewidth=1, linestyle='--', color='red')
 # Add buildings
 ax = buildings.plot(ax=ax, facecolor='khaki', alpha=0.7)
 
 # Add the route
-ax = route_geom.plot(ax=ax, linewidth=1, linestyle='--', color='red')
+# ax = route_geom.plot(ax=ax, linewidth=1, linestyle='--', color='red')
 
 # Add the origin and destination nodes of the route
 ax = dest_nodes.plot(ax=ax, markersize=24, color='green')
